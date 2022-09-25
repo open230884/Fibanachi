05fd3c310d5d24ae7aec5d4e3e0c5ae990d9665d
Andrew PV <112729614+open230884@users.noreply.github.com>
2022-09-25 23:30:22 +0300

    запустил наконец

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿!
+﻿int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+
+
+for (int i = 1; i < 40; i++)
+{
+
+Console.WriteLine(Fibonacci(i));
+} 
